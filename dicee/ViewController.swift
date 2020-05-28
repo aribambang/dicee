00b25baaefd29a07177d99bbf566a0e9a7bc9aac
Ari Bambang Kurniawan <aribk210@gmail.com>
2020-05-29 05:05:27 +0700

    Initial Commit

---
+++ dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dicee
+//
+//  Created by Ari Bambang Kurniawan on 29/05/20.
+//  Copyright © 2020 Ari Bambang Kurniawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
