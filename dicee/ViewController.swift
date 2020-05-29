ad18e4594c1b0dfbf1ca110d65ccf22021dae7e7
Ari Bambang Kurniawan <aribk210@gmail.com>
2020-05-30 00:17:19 +0700

    add random function

--- dicee/ViewController.swift
+++ dicee/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceNumberView: UILabel!
+    
+    var angkaKanan = 0
+    var angkaKiri = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let angkaKanan = Int.random(in: 0...5)
+        let angkaKiri = Int.random(in: 0...5)
+        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")][angkaKanan]
+        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")][angkaKiri]
+        diceNumberView.text = "\(angkaKanan+angkaKiri+2)"
+        
+    }
+    
 }
 
